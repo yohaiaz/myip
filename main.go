80d8877010ab2ea198162a1becaf3ae91930a13f
yohai.azriel <yohai.azriel@ironsrc.com>
2018-07-25 09:44:59 +0300

    version 1.0.0

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+
+	graceFullShutDown := make(chan os.Signal, 1)
+
+	signal.Notify(graceFullShutDown)
+
+	fmt.Println("starting server ...")
+
+	http.HandleFunc("/", serverHandler)
+
+	http.ListenAndServe("localhost:5000", nil)
+
+	<-graceFullShutDown
+}
+
+func serverHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprint(w, "welcome, \n")
+
+	fmt.Fprintf(w, "now: %v utc \n", time.Now().UTC())
+
+	fmt.Fprintf(w, "your address is: %s \n", r.RemoteAddr)
+}
